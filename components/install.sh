6a471eb729b49abbbff37c24c88bcb103105c04b
Ducca <contact@icohigh.net>
2021-06-01 21:58:23 +0300

    add install components

---
+++ components/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y \
+&& sudo apt install docker.io curl -y \
+&& sudo systemctl start docker \
+&& sudo systemctl enable docker \
+&& sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose \
+&& sudo chmod +x /usr/local/bin/docker-compose \
+&& sudo docker-compose --version \
+&& sudo apt-get install build-essential ocaml ocamlbuild automake autoconf libtool wget python libssl-dev git cmake make perl tmux ufw gcc unzip zip jq -y
+
+echo "--------ALL COMPONENTS INSTALLED--------"
