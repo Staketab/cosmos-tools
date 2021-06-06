776d43bbb7ea78faa2501b5f8761f95a24776e1c
Ducca <contact@icohigh.net>
2021-06-06 15:34:40 +0300

    logo update

--- components/install.sh
+++ components/install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+echo ""
+echo "░██████╗████████╗░█████╗░██╗░░██╗███████╗████████╗░█████╗░██████╗░░░░░█████╗░░█████╗░███╗░░░███╗"
+echo "██╔════╝╚══██╔══╝██╔══██╗██║░██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗░░░██╔══██╗██╔══██╗████╗░████║"
+echo "╚█████╗░░░░██║░░░███████║█████═╝░█████╗░░░░░██║░░░███████║██████╦╝░░░██║░░╚═╝██║░░██║██╔████╔██║"
+echo "░╚═══██╗░░░██║░░░██╔══██║██╔═██╗░██╔══╝░░░░░██║░░░██╔══██║██╔══██╗░░░██║░░██╗██║░░██║██║╚██╔╝██║"
+echo "██████╔╝░░░██║░░░██║░░██║██║░╚██╗███████╗░░░██║░░░██║░░██║██████╦╝██╗╚█████╔╝╚█████╔╝██║░╚═╝░██║"
+echo "╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░╚═╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝"
+echo ""
+
+sleep 1
+
+GREEN="\033[32m"
+
 sudo apt update && sudo apt upgrade -y \
 && sudo apt install docker.io curl -y \
 && sudo systemctl start docker \
@@ -9,4 +22,7 @@ sudo apt update && sudo apt upgrade -y \
 && sudo docker-compose --version \
 && sudo apt-get install build-essential ocaml ocamlbuild automake autoconf libtool wget python libssl-dev git cmake make perl tmux ufw gcc unzip zip jq -y
 
-echo "--------ALL COMPONENTS INSTALLED--------"
+
+echo "---------------"
+echo -e "$GREEN ALL COMPONENTS INSTALLED.\033[0m"
+echo "---------------"
