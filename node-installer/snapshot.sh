af1c9542263f7a5732915e179c8607c38afd4e35
Ducca <contact@icohigh.net>
2021-10-21 20:43:14 +0300

    added new snapshots
    oasis-node,impacthub-3,panacea-3,juno-1

--- node-installer/snapshot.sh
+++ node-installer/snapshot.sh
@@ -4,7 +4,7 @@ RED="\033[31m"
 YELLOW="\033[33m"
 GREEN="\033[32m"
 NORMAL="\033[0m"
-SNAP_BINARIES="akash,sifnoded,sentinelhub,desmos,osmosisd,bcnad,oasis-node,ixod,panacead,junod"
+SNAP_BINARIES="akashnet-2,sifchain,sentinelhub-2,desmos-mainnet,osmosis-1,bitcanna-1,oasis-node,impacthub-3,panacea-3,juno-1"
 
 function line {
 echo "-------------------------------------------------------------------"
@@ -29,27 +29,27 @@ sleep 5
 }
 
 function snapStart {
-if [[ "${BIN_NAME}" == "akash" ]]; then
+if [[ "${BIN_NAME}" == "akashnet-2" ]]; then
     cd $HOME/.${CONFIG_FOLDER}/data
     SNAP_NAME=$(curl -s http://135.181.60.250/akash/ | egrep -o ">akashnet-2.*tar" | tr -d ">")
     wget -O - http://135.181.60.250/akash/${SNAP_NAME} | tar xf -
-elif [[ "${BIN_NAME}" == "sifnoded" ]]; then
+elif [[ "${BIN_NAME}" == "sifchain" ]]; then
     cd $HOME/.${CONFIG_FOLDER}/data
     SNAP_NAME=$(curl -s http://135.181.60.250:8081/sifchain/ | egrep -o ">sifchain.*tar" | tail -n 1 | tr -d '>')
     wget -O - http://135.181.60.250:8081/sifchain/${SNAP_NAME} | tar xf -
-elif [[ "${BIN_NAME}" == "sentinelhub" ]]; then
+elif [[ "${BIN_NAME}" == "sentinelhub-2" ]]; then
     cd $HOME/.${CONFIG_FOLDER}/data
     SNAP_NAME=$(curl -s http://135.181.60.250:8083/sentinel/ | egrep -o ">sentinelhub-2.*tar" | tr -d ">")
     wget -O - http://135.181.60.250:8083/sentinel/${SNAP_NAME} | tar xf -
-elif [[ "${BIN_NAME}" == "desmos" ]]; then
+elif [[ "${BIN_NAME}" == "desmos-mainnet" ]]; then
     cd $HOME/.${CONFIG_FOLDER}/data
     SNAP_NAME=$(curl -s http://135.181.60.250:8084/desmos/ | egrep -o ">desmos.*tar" | tr -d ">")
     wget -O - http://135.181.60.250:8084/desmos/${SNAP_NAME} | tar xf -
-elif [[ "${BIN_NAME}" == "osmosisd" ]]; then
+elif [[ "${BIN_NAME}" == "osmosis-1" ]]; then
     cd $HOME/.${CONFIG_FOLDER}/data
     SNAP_NAME=$(curl -s http://135.181.60.250:8085/osmosis/ | egrep -o ">osmosis.*tar" | tr -d ">")
     wget -O - http://135.181.60.250:8085/osmosis/${SNAP_NAME} | tar xf -
-elif [[ "${BIN_NAME}" == "bcnad" ]]; then
+elif [[ "${BIN_NAME}" == "bitcanna-1" ]]; then
     cd $HOME/.${CONFIG_FOLDER}/data
     SNAP_NAME=$(curl -s http://135.181.60.250:8086/bitcanna/ | egrep -o ">bitcanna.*tar" | tr -d ">")
     wget -O - http://135.181.60.250:8086/bitcanna/${SNAP_NAME} | tar xf -
@@ -57,15 +57,15 @@ elif [[ "${BIN_NAME}" == "oasis-node" ]]; then
     cd $HOME/.${CONFIG_FOLDER}/data
     SNAP_NAME=$(curl -s http://95.216.45.250:8081/ | egrep -o ">oasis.*tar" | tr -d ">")
     wget -O - http://95.216.45.250:8081/${SNAP_NAME} | tar xzf -
-elif [[ "${BIN_NAME}" == "ixod" ]]; then
+elif [[ "${BIN_NAME}" == "impacthub-3" ]]; then
     cd $HOME/.${CONFIG_FOLDER}/data
     SNAP_NAME=$(curl -s http://cosmos-snap.staketab.com/ixo/ | egrep -o ">impacthub-3.*tar" | tr -d ">")
     wget -O - http://cosmos-snap.staketab.com/ixo/${SNAP_NAME} | tar xf -
-elif [[ "${BIN_NAME}" == "panacead" ]]; then
+elif [[ "${BIN_NAME}" == "panacea-3" ]]; then
     cd $HOME/.${CONFIG_FOLDER}/data
     SNAP_NAME=$(curl -s http://cosmos-snap.staketab.com/medibloc/ | egrep -o ">panacea-3.*tar" | tr -d ">")
     wget -O - http://cosmos-snap.staketab.com/medibloc/${SNAP_NAME} | tar xf -
-elif [[ "${BIN_NAME}" == "junod" ]]; then
+elif [[ "${BIN_NAME}" == "juno-1" ]]; then
     cd $HOME/.${CONFIG_FOLDER}/data
     SNAP_NAME=$(curl -s https://snapshots.stakecraft.com/ | egrep -o ">juno-1.*tar" | tr -d ">")
     wget -O - https://snapshots.stakecraft.com/${SNAP_NAME} | tar xf -
