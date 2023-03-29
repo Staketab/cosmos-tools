58cca1115bed43d53e622e7b06c24782657f83ab
Ducca <64695363+icohigh@users.noreply.github.com>
2023-03-29 11:54:39 +0300

    fix statesync for sei custom network

--- state-sync/statesync.sh
+++ state-sync/statesync.sh
@@ -43,7 +43,10 @@ backupQuestion() {
 }
 remove() {
   cp -r $HOME/${CONFIG_FOLDER}/data/priv_validator_state.json $HOME/${CONFIG_FOLDER}/priv_validator_state.json
-  if [ -e $HOME/${CONFIG_FOLDER}/priv_validator_state.json ]; then
+  if [ ${BINARY} == "seid" ]; then
+    echo -e "$YELLOW Resetting chain data...$NORMAL"
+    sudo rm -rf $HOME/${CONFIG_FOLDER}/data/*
+  elif [ ${BINARY} != "seid" ]; then
     echo -e "$YELLOW Resetting chain data...$NORMAL"
     ${BINARY} tendermint unsafe-reset-all --home $HOME/${CONFIG_FOLDER} --keep-addr-book
   else
@@ -75,16 +78,38 @@ statesync() {
   sed -i.bak -E 's#^(rpc_servers[[:space:]]+=[[:space:]]+).*$#\1"'$RPC','$RPC'"#' $HOME/${CONFIG_FOLDER}/config/config.toml
   sed -i.bak -E 's#^(trust_height[[:space:]]+=[[:space:]]+).*$#\1"'$TRUST_HEIGHT'"#' $HOME/${CONFIG_FOLDER}/config/config.toml
   sed -i.bak -E 's#^(trust_hash[[:space:]]+=[[:space:]]+).*$#\1"'$TRUST_HASH'"#' $HOME/${CONFIG_FOLDER}/config/config.toml
-  #sed -i.bak -E 's#^(trust_period[[:space:]]+=[[:space:]]+).*$#\1"'$TRUST_PERIOD'"#' $HOME/.${CONFIG_FOLDER}/config/config.toml
 
   # STATESYNC enabled
+  sed -i.bak -E 's#^(enable[[:space:]]+=[[:space:]]+).*$#\1'true'#' $HOME/${CONFIG_FOLDER}/config/config.toml
+}
+statesyncOthers() {
+  line
+  LATEST_HEIGHT=$(curl -s ${RPC}/block | jq -r .block.header.height)
+  TRUST_HEIGHT=$((LATEST_HEIGHT - ${BLOCKS}))
+  TRUST_HASH=$(curl -s "${RPC}/block?height=$TRUST_HEIGHT" | jq -r .block_id.hash)
+  echo -e "$YELLOW RPC SERVERS:$NORMAL$RED ${RPC},${RPC}$NORMAL"
+  echo -e "$YELLOW TRUST_HEIGHT:$NORMAL$RED ${TRUST_HEIGHT}$NORMAL"
+  echo -e "$YELLOW TRUST_HASH:$NORMAL$RED ${TRUST_HASH}$NORMAL"
+  line
+  sleep 3
+
+  sed -i.bak -E 's#^(rpc-servers[[:space:]]+=[[:space:]]+).*$#\1"'$RPC','$RPC'"#' $HOME/${CONFIG_FOLDER}/config/config.toml
+  sed -i.bak -E 's#^(trust-height[[:space:]]+=[[:space:]]+).*$#\1"'$TRUST_HEIGHT'"#' $HOME/${CONFIG_FOLDER}/config/config.toml
+  sed -i.bak -E 's#^(trust-hash[[:space:]]+=[[:space:]]+).*$#\1"'$TRUST_HASH'"#' $HOME/${CONFIG_FOLDER}/config/config.toml
+
+  # STATESYNC enabled
+  sed -i.bak -E 's#^(use-p2p[[:space:]]+=[[:space:]]+).*$#\1'true'#' $HOME/${CONFIG_FOLDER}/config/config.toml
   sed -i.bak -E 's#^(enable[[:space:]]+=[[:space:]]+).*$#\1'true'#' $HOME/${CONFIG_FOLDER}/config/config.toml
 }
 
 start() {
   backupQuestion
-  statesync
   copyJson
+  if [ ${BINARY} == "seid" ]; then
+    statesyncOthers
+  else
+    statesync
+  fi
   echo -e "$YELLOW Restarting...$NORMAL"
   sudo systemctl restart ${SERVICE_NAME}
   echo -e "$YELLOW Checking logs...$NORMAL"
